d62334d29445114cd00f20b6bda2f19467a0884c
NguyenHuuYen <huuyen911@gmail.com>
2019-05-22 17:21:45 +0700

    display home or error

---
+++ application/views/home.php
@@ -0,0 +1,7 @@
+<h1>
+    Get title, time, content of the articles on Vnexpress, Vietnamnet, Dantri.
+</h1>
+<h2>
+    <!-- Echo if exist error. -->
+    <?php echo isset($error) ? $error : ''; ?>
+</h2>
